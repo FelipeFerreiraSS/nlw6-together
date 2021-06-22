db04cdaecbd0eee6c5dc43b54a76fb0cfd08f036
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-06-22 00:05:08 -0300

    firebase2

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth'
 import 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDCqDT_lKRn084lemqLqZrwIxTOAsvqJVc",
-  authDomain: "letmeask-web-nlw.firebaseapp.com",
-  databaseURL: "https://letmeask-web-nlw-default-rtdb.firebaseio.com",
-  projectId: "letmeask-web-nlw",
-  storageBucket: "letmeask-web-nlw.appspot.com",
-  messagingSenderId: "565275107430",
-  appId: "1:565275107430:web:fc15b6b35328613a85cda7"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
